4d613ecf9ed779a097006d3e384e30c2685e1878
Bueno-Italo <italo-bueno@hotmail.com>
2026-09-23 22:11:45 -0300

    feat: Application Exception Handling with Middleware

---
+++ School.API/Errors/ApiException.cs
@@ -0,0 +1,15 @@
+﻿namespace School.API.Errors
+{
+    public class ApiException
+    {
+        public ApiException(string statusCode, string message, string details)
+        {
+            StatusCode = statusCode;
+            Message = message;
+            Details = details;
+        }
+        public string StatusCode { get; }
+        public string Message { get; }
+        public string Details { get; }
+    }
+}
---
+++ School.API/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,51 @@
+﻿using School.API.Errors;
+using School.App.Exceptions;
+using System.Net.WebSockets;
+using System.Text.Json;
+
+namespace School.API.Middleware
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ExceptionMiddleware> _logger;
+        private readonly IHostEnvironment _env;
+
+        public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger, IHostEnvironment env)
+        {
+            _next = next;
+            _logger = logger;
+            _env = env;
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            try
+            {
+                await _next(httpContext);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+
+                int statusCode = ex switch
+                {
+                    NotFoundException => StatusCodes.Status404NotFound,
+                    BadRequestException => StatusCodes.Status400BadRequest,
+                    UnauthorizedAccessException => StatusCodes.Status401Unauthorized,
+                    _ => StatusCodes.Status500InternalServerError
+                };
+
+                httpContext.Response.StatusCode = statusCode;
+                httpContext.Response.ContentType = "application/json";
+
+                var response = _env.IsDevelopment()
+                    ? new ApiException(statusCode.ToString(), ex.Message, ex.StackTrace?.ToString())
+                    : new ApiException(statusCode.ToString(), ex.Message, "Internal Server Error");
+                var option = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+                var json = JsonSerializer.Serialize(response, option);
+                await httpContext.Response.WriteAsync(json);
+            }
+        }
+    }
+}
